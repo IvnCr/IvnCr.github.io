ebb6b152c1b09e88037973baf842b09db4c16caa
IvnCr <icr0015@alu.medac.es>
2024-11-25 11:58:20 +0100

    Animaciones

--- index.js
+++ index.js
@@ -38,3 +38,43 @@ window.addEventListener('load', function () {
         }
     });
 });
+
+//HERO
+let currentHeroIndex = 0;
+const heroImages = document.querySelectorAll('.hero-images .slider-image');
+const totalHeroImages = heroImages.length;
+
+function changeHeroImage() {
+    heroImages[currentHeroIndex].classList.remove('active');
+    currentHeroIndex = (currentHeroIndex + 1) % totalHeroImages;
+    heroImages[currentHeroIndex].classList.add('active');
+}
+
+setInterval(changeHeroImage, 3000); // Cambia cada 3 segundos
+
+//CATEGORY
+let currentInvisibleIndex = 0;
+const invisibleImages = document.querySelectorAll('.invisible-box .slider-image');
+const totalInvisibleImages = invisibleImages.length;
+
+function changeInvisibleImage() {
+    invisibleImages[currentInvisibleIndex].classList.remove('active');
+    currentInvisibleIndex = (currentInvisibleIndex + 1) % totalInvisibleImages;
+    invisibleImages[currentInvisibleIndex].classList.add('active');
+}
+
+setInterval(changeInvisibleImage, 3000); // Cambia cada 3 segundos
+
+// BRANDS 
+let currentInvisible2Index = 0;
+const invisible2Images = document.querySelectorAll('.invisible-box2 .slider-image');
+const totalInvisible2Images = invisible2Images.length;
+
+function changeinvisible2Image() {
+    invisible2Images[currentInvisible2Index].classList.remove('active');
+    currentInvisible2Index = (currentInvisible2Index + 1) % totalInvisible2Images;
+    invisible2Images[currentInvisible2Index].classList.add('active');
+}
+
+setInterval(changeinvisible2Image, 3000); // Cambia cada 3 segundos
+
